8f8e75c513d47ad1499273458554f98e73bfd330
Seval Şentürk <sevalsenturk2017@gmail.com>
2022-11-25 16:44:23 +0300

    create blankpage-1

--- panel/application/views/includes/footer.php
+++ panel/application/views/includes/footer.php
@@ -1,1 +1,7 @@
-<?php
+<div class="wrap p-t-0">
+    <footer class="app-footer">
+        <div class="clearfix">
+            <div class="copyright pull-left">Tüm hakları saklıdır... <?=date("y")?> &copy;</div>
+        </div>
+    </footer>
+</div>
