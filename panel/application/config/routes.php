3decce2df7e3486c5b747db5f94db842caef8944
Seval Şentürk <sevalsenturk2017@gmail.com>
2022-12-01 15:49:59 +0300

    viewfolder yapısı

--- panel/application/config/routes.php
+++ panel/application/config/routes.php
@@ -1,5 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'dashboard';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
