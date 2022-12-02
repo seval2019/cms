c1bce4da9c820eefa4192e922ad55a78c2e14a7a
Seval Şentürk <sevalsenturk2017@gmail.com>
2022-12-02 11:32:41 +0300

    ürün listesi ve HTML tablo oluşturma işlemi

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,31 @@
-ürün listesi content...
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">Ürünler Listesi</h4>
+    </div>
+    <div class="col-md-12">
+        <div class="widget p-lg">
+            <table class="table table-striped">
+                <thead>
+                <th>#id</th>
+                <th>Url</th>
+                <th>Başlık</th>
+                <th>Açıklama</th>
+                <th>Durumu</th>
+                <th>İşlem</th>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>#1</td>
+                    <td>HTMI Kablo</td>
+                    <td>HTMI Kablo</td>
+                    <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</td>
+                    <td>1/0</td>
+                    <td>
+                        <a type="button" href="#" class="btn btn-xs btn-danger btn-outline"><i class="fa fa-trash"></i> Sil</a>
+                        <a type="button" href="#" class="btn btn-xs btn-info btn-outline"><i class="fa fa-pencil-square-o"></i> Düzenle</a>
+                </tr>
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div><!-- .row -->
